ade3e52e02eba7369122c81f7df965ebbf470433
albert r. carnier guedes <albert@teko.net.br>
2024-09-27 17:56:59 -0300

    change: add author to post model and generate a better fake content for post.

--- djanblog/management/commands/populate.py
+++ djanblog/management/commands/populate.py
@@ -3,24 +3,48 @@ from djanblog.models import Post
 from faker import Faker
 from datetime import datetime, timedelta
 from random import randint
+from django.contrib.auth.models import User
 
 class Command(BaseCommand):
     help = 'Popula o banco de dados com dados falsos'
 
+    fake = Faker()        
+    
     def handle(self, *args, **kwargs):
-        fake = Faker()
+
         today = datetime.now()
-        for i in range(10):
-            random_days = fake.random_int(min=1, max=365)
-            created = today - timedelta(days=random_days)
+        
+        for i in range(1):
+
+            random_days = self.fake.random_int(min=1, max=365)
+
+            author = User.objects.order_by('?').first()                        
+            title = self.fake.sentence().rstrip('.').lower()
+            description = self.fake.paragraph()
+            content = self.generateContent()        
+            published = randint(0, 1)
+            
             post = Post(
-                title=fake.sentence().rstrip('.').lower(),
-                description=fake.sentence(),
-                content=fake.text(),
-                published=randint(0, 1),
+                author=author,
+                title=title,
+                description=description,
+                content=content,
+                published=published,
             )            
+            
+            created = today - timedelta(days=random_days)
             post.created_at = created
             post.updated_at = created
+            
             post.save() 
             
         self.stdout.write(self.style.SUCCESS('Banco de dados populado com sucesso!'))
+
+    def generateContent(self):
+        content = ''        
+        for i in range(50):
+            content += self.fake.paragraph() + '\n'
+            if randint(i, 50) == i:
+                content += '<br><br>'
+                
+        return content
--- djanblog/models/post.py
+++ djanblog/models/post.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     id = models.BigAutoField(primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=256,null=False,default='')
     description = models.CharField(max_length=512,null=False,default='')
     content = models.TextField(null=False,default='')
